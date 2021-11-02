b42167f91999ebb6657a6ec224651bdedf6e0e26
Jan Niklas Engel <captndata@gmail.com>
2021-11-02 18:41:54 +0100

    Add placeholder for custom number conversion

---
+++ 211103_Uebung03/CustomNumberConversion.java
@@ -0,0 +1,10 @@
+public class CustomNumberConversion {
+    public static void main(String[] args) {
+
+        // binary number
+        String binary = "01011011";
+        System.out.println("Binary representation: " + binary);
+
+    }
+
+}
