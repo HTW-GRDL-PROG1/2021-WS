6e84c72f029ec33c6d371813a3a89f1ee7070377
Jan Niklas Engel <captndata@gmail.com>
2021-11-02 18:38:02 +0100

    Prepare Abgabe03 for class

--- Abgaben/Abgabe03.java
+++ Abgaben/Abgabe03.java
@@ -1,40 +1,79 @@
 public class Abgabe03 {
     public static void main(String[] args) {
 
-        // Frage 1
-        System.out.println(15 / 2);
+        System.out.println("Frage 1: (15 / 2) ?");
+        System.out.println("Antwort: " + "...");
+        System.out.println("Lösung: " + (15 / 2));
+        System.out.println("========================");
 
-        // Frage 2:
-        System.out.println((3 / 2) / 2.0);
+        // System.out.println("Frage 2: (3 / 2) / 2.0 ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + ((3 / 2) / 2.0));
+        // System.out.println("========================");
 
-        // Frage 3:
-        System.out.println((15 % 7) * 2);
+        // System.out.println("Frage 3: (15 % 7) * 2 ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + ((15 % 7) * 2));
+        // System.out.println("========================");
 
-        // Frage 4:
-        double preis;
-        int vier = 4;
-        boolean b = false, weiter = true;
+        // // Frage 4:
+        // double preis;
+        // int vier = 4;
+        // boolean b = false, weiter = true;
 
-        System.out.println(b || !(b && weiter));
-        System.out.println(preis = (vier + 3) / 2.0);
-        System.out.println(!(b == false));
-        // System.out.println(b == true = false);
-        System.out.println((preis = 4.9f) != vier);
-        System.out.println((vier + 3) / 2);
-        System.out.println(vier / 0.4);
-        System.out.println(b = false != weiter);
+        // System.out.println("Frage 4a: b || !(b && weiter) ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println(b || !(b && weiter));
+        // System.out.println("------------------------");
 
-        // Frage 5:
-        System.out.println(Integer.toBinaryString(3));
+        // System.out.println("Frage 4b: preis = (vier + 3) / 2.0 ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println(preis = (vier + 3) / 2.0);
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4c: !(b == false) ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println(!(b == false));
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4d: b == true = false ?");
+        // System.out.println("Antwort: " + "...");
+        // // System.out.println(b == true = false);
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4e: (preis = 4.9f) != vier ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + ((preis = 4.9f) != vier));
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4f: (vier + 3) / 2 ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + (vier + 3) / 2);
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4g: vier / 0.4 ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + vier / 0.4);
+        // System.out.println("------------------------");
+
+        // System.out.println("Frage 4h: b = false != weiter ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + (b = false != weiter));
+        // System.out.println("========================");
+
+        // System.out.println("Frage 5: 3 als Binärzahl ?");
+        // System.out.println("Antwort: " + "...");
+        // System.out.println("Lösung: " + Integer.toBinaryString(3));
+        // System.out.println("========================");
 
         // Frage 6:
-        // @ -> char
-        // Telefonnummer -> String
-        // Reelle Zahl -> double
-        // Stückzahl -> int
-        // Lichtschalter -> boolean
-        // Gewicht -> double
-        // Zeichen 9 -> char
+        // @ ->
+        // Telefonnummer ->
+        // Reelle Zahl ->
+        // Stückzahl ->
+        // Lichtschalter ->
+        // Gewicht ->
+        // Zeichen 9 ->
 
     }
 }
